7a19b74c43d9c16a877dcceb1dcc350ac5050b22
svc-student <svc-student@student.directory.jccc>
2018-02-27 17:50:10 -0600

    Audio demo basics

---
+++ Assets/AudioControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour {
+
+    private AudioSource myAudioSource;
+
+	// Use this for initialization
+	void Start () {
+        myAudioSource = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            // toggle play
+            if (myAudioSource.isPlaying)
+                myAudioSource.Stop();
+            else
+                myAudioSource.Play();
+        }
+
+	}
+}
---
+++ Assets/SimpleWASD.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleWASD : MonoBehaviour {
+
+
+    public float speed = 5.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 moveDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) moveDirection += Vector3.forward;
+        if (Input.GetKey(KeyCode.A)) moveDirection -= Vector3.right;
+        if (Input.GetKey(KeyCode.S)) moveDirection -= Vector3.forward;
+        if (Input.GetKey(KeyCode.D)) moveDirection += Vector3.right;
+
+        transform.position += moveDirection.normalized * speed * Time.deltaTime;
+
+    }
+}
